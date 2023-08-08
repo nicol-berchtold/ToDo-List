33585cc5710cd70aa2a784dba5a438c3ccabb139
nicol-berchtold <berchtold.nicol@gmail.com>
2023-08-08 13:24:24 +0200

    first push

---
+++ dist/main.js
@@ -0,0 +1,8 @@
+"use strict";
+class ProjectInput {
+    constructor() {
+        this.templateElement = document.getElementById("create-ToDo");
+        this.parentElement = document.getElementById("");
+    }
+}
+//# sourceMappingURL=main.js.map
---
+++ src/main.ts
@@ -0,0 +1,12 @@
+class ProjectInput {
+    templateElement: HTMLTemplateElement;
+    parentElement: HTMLDivElement;
+    element: HTMLInputElement;
+
+    constructor() {
+        this.templateElement = document.getElementById("create-ToDo")! as HTMLTemplateElement;
+        this.parentElement = document.getElementById("")
+
+    }
+
+}
